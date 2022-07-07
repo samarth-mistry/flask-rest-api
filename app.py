fdadea78759bbdf0bc6b273ed9c98aedabdf9406
Samarth Mistry <samarthmistry13@outlook.com>
2022-07-07 19:47:51 +0530

    Added app.py

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask
+
+app=Flask(__name__)
+
+stores=[
+    {
+        "name": "DD Store",
+        "items": [
+            {
+                "name": "Item 1",
+                "price": 16.99
+            }
+        ]
+    },
+    {
+        "name": "City Sq. Mart",
+        "items": [
+            {
+                "name": "Item 2",
+                "price": 993.99
+            }
+        ]
+    }
+]
+
+@app.route('/')
+def landing():
+    return "LANDING!"
+
+@app.route('/store',methods=['POST'])
+def createStore():
+    pass
+
+@app.route('/store/<string:name>')
+def getStore(name):
+    pass
+
+@app.route('/store')
+def getStores():
+    pass
+
+@app.route('/store/<string:name>/item',methods=['POST'])
+def createItemInStore(name):
+    pass
+
+@app.route('/store/<string:name>/item')
+def getItemsInStore(name):
+    pass
+
+app.run(port=5000)
