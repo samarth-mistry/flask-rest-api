ed59317c771d8243a106cfec8a1f0c578efcf994
Samarth Mistry <samarthmistry13@outlook.com>
2022-07-08 10:13:50 +0530

    ItemList using loop

--- section4/app.py
+++ section4/app.py
@@ -1,12 +1,32 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
-class Student(Resource):
-    def get(self, name):
-        return {'student': name}
+items = []
 
-api.add_resource(Student, '/student/<string:name>')
-app.run(port=5000)
+class Item(Resource):
+    def get(self, name):
+        for item in items:
+            if item['name'] == name:
+                return item
+        return {'item': None}, 404
+        return {'message': 'No match found!'}
+
+    def post(self, name):
+        data = request.get_json()
+        item = {'name': name, 'price': data['price']}
+        items.append(item)
+        return item, 201
+
+        #202 accepted
+        #200 ok
+
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+app.run(port=5000, debug=True)
